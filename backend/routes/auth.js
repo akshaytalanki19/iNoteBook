60304178bd6c7c09b54fa2b01b33c7195805de38
AKSHAY KUMAR <akshaytalanki19@gmail.com>
2025-01-20 23:19:03 +0530

    implemented token based authentication, introduced login endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,14 @@
 import { Router } from "express"
 import User from "../models/User.js"
 import { body, validationResult } from "express-validator"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv";
+
+dotenv.config();
 const router = Router();
+const JWT_SECRET = process.env.JWT_TOKEN;
+
 //create a User using: POST "/api/auth"
 router.post('/createUser',[
      body('name', 'Name should be min 3 characters').isLength({min:3}),
@@ -17,15 +24,60 @@ router.post('/createUser',[
     if(user){
         return res.status(400).json({error: "user already exists"})
     }
+    const salt = await bcrypt.genSalt(10);
+    const secPass =await bcrypt.hash(req.body.password, salt);
     user= await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email,
-    })
+    });
+    const data ={
+        user:{
+        id: user.id
+        }
+    }
+    const jwtData=jwt.sign(data,JWT_SECRET);
+    console.log(jwtData);
+    res.json(jwtData);
    }catch(error){
      console.log(error.message);
      res.status(500).send("Internal server error");
    }
 })
 
+
+// authenticate a user
+router.post('/login',[
+    body('email', 'Enter a valid name').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+],async (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {email, password}=req.body;
+
+    try{
+        let user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error: "Please enter valid credentials"});
+        }
+        const passwordCompare =await bcrypt.compare(password, user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error: "Please enter valid credentials"});
+        }
+        const data ={
+            user:{
+            id: user.id
+            }
+        }
+        const jwtData=jwt.sign(data,JWT_SECRET);
+        res.json(jwtData);
+    }catch(error){
+        console.log(error.message);
+        res.status(500).send("Internal server error");
+    }
+})
+  
 export default router;
