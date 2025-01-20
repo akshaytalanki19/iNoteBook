79a3407aa68fbca08d7fa23c75b78a897df73b3b
AKSHAY KUMAR <akshaytalanki19@gmail.com>
2025-01-20 09:50:20 +0530

    implemented createUSer endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,24 +3,29 @@ import User from "../models/User.js"
 import { body, validationResult } from "express-validator"
 const router = Router();
 //create a User using: POST "/api/auth"
-router.post('/',[
+router.post('/createUser',[
      body('name', 'Name should be min 3 characters').isLength({min:3}),
      body('email', 'Enter a valid name').isEmail(),
      body('password', 'Password should be min 5 characters').isLength({min:5}),
-],(req,res)=>{
+],async (req,res)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
     }
-    User.create({
+    try{
+    let user =await User.findOne({email: req.body.email});
+    if(user){
+        return res.status(400).json({error: "user already exists"})
+    }
+    user= await User.create({
         name: req.body.name,
         password: req.body.password,
         email: req.body.email,
-    }).then(user=> res.json(user))
-    .catch(err=> {console.log(err)
-       res.json({error: 'email already exists'})
-     })
-   
+    })
+   }catch(error){
+     console.log(error.message);
+     res.status(500).send("Internal server error");
+   }
 })
 
 export default router;
