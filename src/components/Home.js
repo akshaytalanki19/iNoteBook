e386e279cfd5356cac9299f6044a0e89880de847
AKSHAY KUMAR <akshaytalanki19@gmail.com>
2025-02-04 23:18:16 +0530

    change ui

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,7 @@
-import React, {useContext} from 'react'
-import noteContext from '../context/notes/NoteContext'
+import React from 'react'
+import Notes from './Notes';
 export const Home = () => {
-  const context = useContext(noteContext);
-  const {notes, setNotes} = context;
+
   return (
     <div>
     <div className='container my-3'>
@@ -24,13 +23,7 @@ export const Home = () => {
           <button type="submit" class="btn btn-primary">Submit</button>
        </form>
     </div>
-    <div className="container my-3">
-      <h2>your Notes</h2>
-      {notes.map((note)=>{
-        return note.title
-      })
-    }
-    </div>
+     <Notes/>
     </div>
   )
 }
